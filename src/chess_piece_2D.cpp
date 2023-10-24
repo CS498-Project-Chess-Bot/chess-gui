b282768af3f405f853641c99f536bb352021fa7f
Matthew Whalen <mrwhalen22@gmail.com>
2023-10-24 13:39:31 -0400

    full board with all pieces, needed texture manager

--- src/chess_piece_2D.cpp
+++ src/chess_piece_2D.cpp
@@ -1,12 +1,22 @@
 #include "chess_piece_2D.hpp"
 #include "buffer.hpp"
+#include "texture_loader.hpp"
 
 static std::string getChessPieceFragShaderLoc(ChessPieceType type) {
     switch(type) {
+        case ChessPieceType::white_king:
+        case ChessPieceType::white_queen:
+        case ChessPieceType::white_rook:
+        case ChessPieceType::white_knight:
+        case ChessPieceType::white_bishop:
         case ChessPieceType::white_pawn:
         case ChessPieceType::black_king:
+        case ChessPieceType::black_queen:
+        case ChessPieceType::black_rook:
+        case ChessPieceType::black_knight:
+        case ChessPieceType::black_bishop:
+        case ChessPieceType::black_pawn:
             return "shaders/chess_piece.fs";
-            break;
         default:
             CORE_ASSERT(false, "This chess piece doesn't have a fragment shader!");
             return "";
@@ -15,10 +25,19 @@ static std::string getChessPieceFragShaderLoc(ChessPieceType type) {
 
 static std::string getChessPieceVertShaderLoc(ChessPieceType type) {
     switch(type) {
+        case ChessPieceType::white_king:
+        case ChessPieceType::white_queen:
+        case ChessPieceType::white_rook:
+        case ChessPieceType::white_knight:
+        case ChessPieceType::white_bishop:
         case ChessPieceType::white_pawn:
         case ChessPieceType::black_king:
+        case ChessPieceType::black_queen:
+        case ChessPieceType::black_rook:
+        case ChessPieceType::black_knight:
+        case ChessPieceType::black_bishop:
+        case ChessPieceType::black_pawn:
             return "shaders/chess_piece.vs";
-            break;
         default:
             CORE_ASSERT(false, "This chess piece doesn't have a fragment shader!");
             return "";
@@ -27,11 +46,30 @@ static std::string getChessPieceVertShaderLoc(ChessPieceType type) {
 
 static std::string getChessPieceTextureLoc(ChessPieceType type) {
     switch(type) {
+        case ChessPieceType::white_king:
+            return "chess_white_king.png";
+        case ChessPieceType::white_queen:
+            return "chess_white_queen.png";
+        case ChessPieceType::white_rook:
+            return "chess_white_rook.png";
+        case ChessPieceType::white_knight:
+            return "chess_white_knight.png";
+        case ChessPieceType::white_bishop:
+            return "chess_white_bishop.png";
         case ChessPieceType::white_pawn:
-            return "textures/chess_white_pawn.png";
-            break;
+            return "chess_white_pawn.png";
         case ChessPieceType::black_king:
-            return "textures/chess_black_king.png";
+            return "chess_black_king.png";
+        case ChessPieceType::black_queen:
+            return "chess_black_queen.png";
+        case ChessPieceType::black_rook:
+            return "chess_black_rook.png";
+        case ChessPieceType::black_knight:
+            return "chess_black_knight.png";
+        case ChessPieceType::black_bishop:
+            return "chess_black_bishop.png";
+        case ChessPieceType::black_pawn:
+            return "chess_black_pawn.png";
         default:
             CORE_ASSERT(false, "This chess piece doesn't have a fragment shader!");
             return "";
@@ -62,7 +100,7 @@ ChessPieceModel2D::ChessPieceModel2D(ChessPieceType type)
 
     m_shader = createRef<Shader>(getChessPieceVertShaderLoc(type).c_str(), getChessPieceFragShaderLoc(type).c_str());
     
-    m_texture = createRef<Texture>(getChessPieceTextureLoc(type).c_str());
+    m_texture = TextureLoader::GetInstance()->retrieveTexture(getChessPieceTextureLoc(type));
     m_shader->setTexture("textureSlot", *m_texture);
 
 }
